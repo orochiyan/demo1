e1b66074acb107628990b9506f853868e10bc3ff
Hao Yan <hao.yan2011@gmail.com>
2018-09-30 12:03:16 +1000

    Add Router

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Dashboard />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Redirect path="/" to="/dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
+  )
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <Dashboard />
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
