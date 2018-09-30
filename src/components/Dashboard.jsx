9fabf59858dabcbe0ef85e0f6566e93f4219de7c
Hao Yan <hao.yan2011@gmail.com>
2018-09-30 11:15:35 +1000

    Add Dashboard

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Dashboard extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        Dashboard
+      </div>
+    );
+  }
+}
+export default Dashboard;
